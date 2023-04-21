fcc3815897527947ea0fad7cc4c44e451b038abf
Nilton Vasques <nilton.vasques@gmail.com>
2023-04-21 01:25:05 -0300

    add date tests

--- tests.sh
+++ tests.sh
@@ -56,6 +56,10 @@ test_eval "true and false" "false"
 test_eval "true or false" "true"
 test_eval "(3==3) and (3!=3)" "false"
 test_eval "exp(1) == e" "true"
+test_eval '((0.09/1.0)+2.58)-1.67' '1'
+test_eval '10^log(3+2)' '40.6853365119738'
+test_eval 'log(e)' '1'
+test_eval '2^5^0' '2'
 
 # String tests
 test_eval "length(\"test string\")" "11"
@@ -65,14 +69,53 @@ test_eval "concat(\"Hello \", \"World\")" '"Hello World"'
 test_eval "concat(\"\", \"Hello World\")" '"Hello World"'
 test_eval "concat(\"Hello World\", \"\")" '"Hello World"'
 test_eval "concat(concat(\"tes t\", \" \\\"str \\\\\\\" \\\" ing \"),string(\"equa  \\\"   tion\"))" '"tes t "str \" " ing equa  "   tion"'
-test_eval "str2number(\"5\")" "5"
-test_eval "number(\"5\")" "5"
 test_eval "left(\"Hello World\", 5)" '"Hello"'
 test_eval "right(\"Hello World\", 5)" '"World"'
+test_eval 'left("Hello World", 5)' '"Hello"'
+test_eval 'right("Hello World", 5)' '"World"'
+test_eval 'right("Hello World", 20)' '"Hello World"'
+test_eval 'contains("Hello World", "orld")' 'true'
+test_eval 'contains("One Flew Over The Cuckoo'"'"'s", "koo")' 'true'
+test_eval 'contains("Hello World", "Worlds")' 'false'
+test_eval 'contains("1234567", "456")' 'true'
+test_eval 'contains("1234567", "789")' 'false'
+test_eval 'contains("2019-01-01T:08:30", "2019-01-01")' 'true'
+test_eval 'contains("2019-01-01T:08:30", "2021-01-01")' 'false'
+test_eval '"Maçã"' '"Maçã"'
+test_eval '"string with quote\""' '"string with quote""'
+test_eval 'toupper(concat("hello ", "world"))' '"HELLO WORLD"'
+test_eval 'tolower("TEST LOWERCASE")' '"test lowercase"'
 
 # Array tests
 test_eval "link(\"Title\", \"http://foo.bar\")" '"<a href="http://foo.bar">Title</a>"'
 test_eval 'link("Title", "#")' '"<a href="#">Title</a>"'
+
+test_eval 'link("Test title", "/test")' '"<a href="/test">Test title</a>"'
+test_eval 'link("Test title", "/test", "testFileName")' '"<a href="/test" download="testFileName">Test title</a>"'
+
+test_eval '5 > 3 ? "yes" : "no"' '"yes"'
+test_eval '"this" == "this" ? 1 : 0' '1'
+test_eval 'sqrt(9)+cbrt(8)+abs(-4.9812)' '9.9812'
+test_eval '10! - 5! * -(-1)' '3628680'
+test_eval 'sum(1,2,3,4,5) == max(14.99, 15)' 'true'
+test_eval 'avg(1,2,3,4,5,6,7,8,9,10) / 10' '0.55'
+test_eval 'round(4.62)' '5'
+test_eval 'round_decimal(4.625, 2)' '4.63'
+
+# Cast tests
+test_eval "str2number(\"5\")" "5"
+test_eval "number(\"5\")" "5"
+test_eval 'string(5)' '"5"'
+test_eval 'string(5.123)' '"5.123"'
+
+# Date tests
+test_eval 'current_date()' '"'$(date '+%Y-%m-%d')'"'
+test_eval 'daysdiff("2018-01-01", "2018-12-31")' '364'
+test_eval 'daysdiff("2016-01-01", "2016-12-31")' '365'
+test_eval 'daysdiff("2000-01-01", "2000-12-31")' '365'
+test_eval 'daysdiff("2100-01-01", "2100-12-31")' '364'
+test_eval 'daysdiff("2018-01-01", "2017-12-31")' '1'
+
 
 
 echo "All tests passed!"
